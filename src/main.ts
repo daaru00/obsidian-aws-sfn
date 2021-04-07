3dae045bbc9f671bd172c84c05a4b2a67980846d
Fabio Gollinucci <fbgolly@gmail.com>
2021-04-07 23:14:23 +0200

    Optimize block reveal event

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import SettingTab from './settings-tab'
 export default class AwsSfnPlugin extends Plugin {
 	settings: PluginSettings
 	graphs: Graph[]
+	scrollTimeout: number
 
 	async onload(): Promise<void> {
 		await this.loadSettings();
@@ -28,26 +29,8 @@ export default class AwsSfnPlugin extends Plugin {
 				}
 				
 				this.registerDomEvent(markdownPreview, 'scroll', () => {
-					const graphs = this.getAllGraphs()
-					for (const graph of graphs) {
-						if (IsGraphVisible(graph)) {
-							if (graph.classList.contains('.visible')) {
-								continue
-							}
-
-							graph.removeClass('.hidden')
-							graph.addClass('.visible')
-
-							this.onBlockAppear(graph)
-						} else {
-							if (graph.classList.contains('.hidden')) {
-								continue
-							}
-
-							graph.removeClass('.visible')
-							graph.addClass('.hidden')
-						}
-					}
+					window.clearTimeout(this.scrollTimeout)
+					window.setTimeout(this.onScroll.bind(this), 100)
 				})
 			})
 		)
@@ -80,38 +63,35 @@ export default class AwsSfnPlugin extends Plugin {
 			return
 		}
 
-		const widget = this.createWidget(graph)
+		const widget = createWidget(graph)
 		container.appendChild(widget)
+	}
+
+	onScroll(): void {
+		const graphs = this.getAllGraphs()
+		for (const graph of graphs) {
+			if (IsGraphVisible(graph)) {
+				if (graph.classList.contains('.visible')) {
+					continue
+				}
+
+				graph.removeClass('.hidden')
+				graph.addClass('.visible')
+
+				this.onBlockAppear(graph)
+			} else {
+				if (graph.classList.contains('.hidden')) {
+					continue
+				}
+
+				graph.removeClass('.visible')
+				graph.addClass('.hidden')
+			}
+		}
 	}
 
 	onBlockAppear(el: HTMLElement): void {
 		el.dispatchEvent(new Event('redraw'))
-		//console.log('a wild block appeared!', el)
-	}
-
-	createWidget(graph: Graph): HTMLElement {
-		const widget = document.createElement("div");
-		widget.addClass('aws-sfn-widget')
-
-		new ButtonComponent(widget)
-			.setButtonText("\u29BF")
-			.onClick(async () => {
-				graph.renderStateMachine()
-			})
-
-		new ButtonComponent(widget)
-			.setButtonText("\u002B")
-			.onClick(async () => {
-				graph.zoomIn()
-			})
-			
-		new ButtonComponent(widget)
-			.setButtonText("\u2212")
-			.onClick(async () => {
-				graph.zoomOut()
-			})
-
-		return widget
 	}
 
 	postProcessor(): void {
@@ -145,4 +125,29 @@ export default class AwsSfnPlugin extends Plugin {
 				.forEach(graph => graph.removeClass('colorized'))
 		}
 	}
+}
+
+function createWidget(graph: Graph): HTMLElement {
+	const widget = document.createElement("div");
+	widget.addClass('aws-sfn-widget')
+
+	new ButtonComponent(widget)
+		.setButtonText("\u29BF")
+		.onClick(async () => {
+			graph.renderStateMachine()
+		})
+
+	new ButtonComponent(widget)
+		.setButtonText("\u002B")
+		.onClick(async () => {
+			graph.zoomIn()
+		})
+		
+	new ButtonComponent(widget)
+		.setButtonText("\u2212")
+		.onClick(async () => {
+			graph.zoomOut()
+		})
+
+	return widget
 }
